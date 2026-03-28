69087f0193467914245f0fb897d749ad1fcdfca8
“Simar <kalrasimar750@gmail.com>
2026-03-28 23:25:53 +0530

    fix cors production

--- src/main/java/com/capestone/grievance/GrievanceApplication.java
+++ src/main/java/com/capestone/grievance/GrievanceApplication.java
@@ -22,7 +22,7 @@ public class GrievanceApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("http://localhost:3000")
+					.allowedOrigins("*")
 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 					.allowedHeaders("*")
 					.allowCredentials(true);
