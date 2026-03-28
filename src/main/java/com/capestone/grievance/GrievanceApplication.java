3823e3e4a8d4b0fd90502ed1557b245a99d7c245
“Simar <kalrasimar750@gmail.com>
2026-03-28 23:50:40 +0530

    change origin

--- src/main/java/com/capestone/grievance/GrievanceApplication.java
+++ src/main/java/com/capestone/grievance/GrievanceApplication.java
@@ -22,7 +22,7 @@ public class GrievanceApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedOrigins("*")
+				.allowedOriginPatterns("*")
 					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 					.allowedHeaders("*")
 					.allowCredentials(true);
